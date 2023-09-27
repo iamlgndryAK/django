2a86f96ea9446cc4cf5a04c43a4c7d1651f50d20
iamlgndryAK <aadilkhan085529@gmail.com>
2023-09-27 21:39:53 +0530

    Delete Club directory

--- Club/event/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Person
-from .models import Location
-from .models import Event
-
-
-admin.site.register(Person)
-admin.site.register(Location)
-admin.site.register(Event)
